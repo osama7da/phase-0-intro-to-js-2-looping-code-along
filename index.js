79cfdb01089b22f60dce82ce263785e15e9d65fd
Osama daoud <osamadaoud66@gmail.com>
2024-01-03 03:02:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards (array,motion){
+    const newArr=[];
+    for (let i =0 ; i<array.length; i++){
+        newArr.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+    }
+    return newArr;
+}
+ 
+function countDown (number){
+    while(number>=0){ 
+        console.log(number)
+        number--;
+    }
+    return countDown
+}
